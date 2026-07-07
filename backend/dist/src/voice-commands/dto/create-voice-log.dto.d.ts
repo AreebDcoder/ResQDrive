53eeb3d9b04c56f21c801786453a71ccafc522b9
KAMRANkami313 <muhammadkamrankami313@gmail.com>
2026-07-07 13:29:30 +0500

    chore: untrack backend/dist/voice-commands files

--- backend/dist/src/voice-commands/dto/create-voice-log.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { VoiceIntent } from '@prisma/client';
-export declare class CreateVoiceLogDto {
-    incidentId?: string;
-    rawTranscript: string;
-    classifiedIntent: VoiceIntent;
-    recognitionEngine: string;
-    actionTaken: boolean;
-}
