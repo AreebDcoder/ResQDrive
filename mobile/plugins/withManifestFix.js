923b68f5fda92a752fb31723f4ebf3c2f4e1f5ad
AbdulBasit0909 <232477@students.au.edu.pk>
2026-07-13 11:44:31 +0500

    fix: provide explicit value for appComponentFactory manifest replace; add missing expo-font peer dependency

--- mobile/plugins/withManifestFix.js
+++ mobile/plugins/withManifestFix.js
@@ -2,8 +2,18 @@ const { withAndroidManifest } = require('@expo/config-plugins');
 
 module.exports = function withManifestFix(config) {
   return withAndroidManifest(config, (config) => {
-    const application = config.modResults.manifest.application[0];
+    const manifest = config.modResults.manifest;
+
+    // Ensure the tools namespace is declared
+    if (!manifest['$']['xmlns:tools']) {
+      manifest['$']['xmlns:tools'] = 'http://schemas.android.com/tools';
+    }
+
+    const application = manifest.application[0];
+    // Explicitly set the value we want AND tell the merger to use it
+    application['$']['android:appComponentFactory'] = 'androidx.core.app.CoreComponentFactory';
     application['$']['tools:replace'] = 'android:appComponentFactory';
+
     return config;
   });
 };
